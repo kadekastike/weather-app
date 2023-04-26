1bb79dc3bcd872c9bc7b813cad317be8c8672d55
Kadek Astike <kd.astike@gmail.com>
2023-04-26 08:26:59 +0700

    disable debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ class MyApp extends StatelessWidget {
         ),
       ],
       child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: "Weather",
         home: HomePage(),
       ),
